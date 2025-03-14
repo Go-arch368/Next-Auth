b513717d83607b84ac4a73bb59b95cb3685061ae
Gowtham T <gowthamtj0808@gmail.com>
2025-03-14 16:01:30 +0530

    fixed error

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } from "./routes";
@@ -7,27 +8,28 @@ const { auth } = NextAuth(authConfig);
 export default auth((req) => {
     const { nextUrl } = req;
     const isLoggedIn = !!req.auth;
-     console.log(isLoggedIn)
+    console.log(isLoggedIn);
+
     const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
     const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
     if (isApiAuthRoute) {
-        return null;
+        return NextResponse.next();
     }
 
     if (isAuthRoute) {
         if (isLoggedIn) {
-            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+            return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
         }
-        return null;
+        return NextResponse.next();
     }
 
     if (!isLoggedIn && !isPublicRoute) {
-        return Response.redirect(new URL("/auth/login", nextUrl));
+        return NextResponse.redirect(new URL("/auth/login", nextUrl));
     }
 
-    return null;
+    return NextResponse.next();
 });
 
 // Optionally, don't invoke Middleware on some paths
